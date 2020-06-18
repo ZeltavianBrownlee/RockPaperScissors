1e7b191d6d3a2d66db19fe3f015e359da1a42010
Zeltavian Brownlee <zeltavianbrownlee@gmail.com>
2020-06-17 20:10:41 -0500

    Add project files.

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+
+        static string PlayGame(string one, string two)//(comp, me)
+        {
+            //comp choices
+            if (one == "rock" && two == "paper")
+            {
+                return "a";
+            }
+            else if (one == "rock" && two == "scissors")
+            {
+                return "b";
+            }
+            else if (one == "paper" && two == "scissors")
+            {
+                return "a";
+            }
+            else if (one == "paper" && two == "rock")
+            {
+                return "b";
+            }
+            else if (one == "scissors" && two == "paper")
+            {
+                return "b";
+            }
+            else if (one == "scissors" && two == "rock")
+            {
+                return "a";
+            }
+
+            //player choices
+
+            else if (two == "rock" && one == "paper")
+            {
+                return "a";
+            }
+            else if (two == "rock" && one == "scissors")
+            {
+                return "b";
+            }
+            else if (two == "paper" && one == "scissors")
+            {
+                return "a";
+            }
+            else if (two == "paper" && one == "rock")
+            {
+
+                return "b";
+            }
+            else if (two == "scissors" && one == "paper")
+            {
+
+                return "b";
+            }
+            else if (two == "scissors" && one == "rock")
+            {
+
+                return "a";
+            }
+            else if (one == "rock" && two == "rock")
+            {
+                return "c";
+            }
+            else if (one == "scissors" && two == "scissors")
+            {
+                return "c";
+            }
+            else if (one == "paper" && two == "paper")
+            {
+                return "c";
+            }
+            else
+                return "d";
+        }
+
+        static void Main(string[] args)
+        {
+            //declare and intialize variables
+            Random rnd = new Random();
+            string compChoice = "";
+            string playerChoice = "";
+
+            //do while loop to continue until there is a winner
+            do
+            {
+                //generate random number
+                int compNum = rnd.Next(1, 4);
+
+
+
+                //Prompt for and get player choice
+                Console.WriteLine("Please enter your choice of rock, paper, or scissors");
+
+                playerChoice = Console.ReadLine();
+
+                if (compNum == 1)
+                {
+                    compChoice = "rock";
+                }
+                else if (compNum == 2)
+                {
+                    compChoice = "paper";
+                }
+                else
+                {
+                    compChoice = "scissors";
+                }//end if
+
+
+                //display computer random choice
+                Console.WriteLine("The computer's choice is: {0} ", compChoice);
+
+                //Call play function for game
+                PlayGame(compChoice, playerChoice);
+
+
+                //if statements for game message
+                if (PlayGame(compChoice, playerChoice) == "a")
+                {
+                    Console.WriteLine("You are the winner");
+                }
+                if (PlayGame(compChoice, playerChoice) == "b")
+                {
+                    Console.WriteLine("Computer is the winner");
+                }
+                
+            } while (PlayGame(compChoice, playerChoice) == "c");//end do while loop
+
+                Console.ReadKey();
+        }
+
+    }
+}
+
+
---
+++ Sales Tax/Sales Tax/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales_Tax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Declare variables
+            double purchaseAmount;
+            double stateTax;
+            double countyTax;
+            double totalSales;
+           
+
+            //Prompt for amount to purchase
+            Console.WriteLine("What is the amount of your purchase?");
+
+            //Get purchase amount
+            purchaseAmount = Convert.ToDouble(Console.ReadLine());
+
+            //Display purchaseAmount
+            Console.WriteLine(" The amount of your purchase is : {0} ", purchaseAmount);
+
+            //intialize county tax
+            countyTax = purchaseAmount * .02; 
+
+            //display countyTax
+            Console.WriteLine(" Your county tax is: {0} ", countyTax );
+
+            //Intialize stateTax
+            stateTax = purchaseAmount * .04;
+
+            //Calculate and display state sales tax
+            Console.WriteLine("Your state sales tax is: {0} ", stateTax);
+
+            
+            //intialize totalSales
+            totalSales = purchaseAmount + countyTax + stateTax;
+            
+     
+            //Total Sales
+            Console.WriteLine("Your total purchase amount is: {0}", totalSales);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
